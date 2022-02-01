d89ae059d2c43351bfa0552e76bbe93554a804f2
David Nelson <david.nelson1@uqconnect.edu.au>
2022-02-01 12:09:20 +1000

    Remove stdout messages now that the  background worker problem is fixed.

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -3,9 +3,6 @@ class WorkersController < ApplicationController
     worker_id = params[:id]
     container = SidekiqStatus::Container.load(worker_id)
 
-    puts '------------------------------------------------------------------'
-    puts "container.pct_complete has the value: #{container.pct_complete}"
-    puts '------------------------------------------------------------------'
     respond_to do |format|
       format.json do
         render(json: {
--- app/workers/rule_engine_worker.rb
+++ app/workers/rule_engine_worker.rb
@@ -7,17 +7,11 @@ class RuleEngineWorker
   def perform(resource)
     at(0)    
     deco_errors = serialize_errors_and_filters(resource)[:deco_errors]
-    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    puts "The number of errors is #{deco_errors.count}"
-    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
     self.total = deco_errors.size
     errors_categorised = 0
     deco_errors.each do |deco_error|
       match_error_to_filters(deco_error)
       at(errors_categorised += 1)
-      puts "---------------------------------------------------------------------"
-      puts errors_categorised
-      puts "---------------------------------------------------------------------"
     end
   end
 end
