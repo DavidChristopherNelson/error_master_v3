fc2e9833f9c3d3211ec1fe0b994750676a88f55c
David Nelson <david.nelson1@uqconnect.edu.au>
2022-02-23 12:53:27 +1000

    Attempt to convert from PG::Result to something that can be saved in the cache

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,16 @@ class ApplicationController < ActionController::Base
     sql_rule_command = 'SELECT rules.filter_id, rules.id, rules.field, rules.value FROM filters INNER JOIN rules ON filters.id = rules.filter_id'
     rule_data = Rule.connection.execute(sql_rule_command)
     puts '==================================================================================='
+    puts rule_data
+    puts '==================================================================================='
     puts rule_data.class
     puts '==================================================================================='
-  end
+    puts rule_data.values
+    puts '==================================================================================='
+    puts rule_data.values.class
+    puts '==================================================================================='
+    puts Hash[rule_data.values]
+    puts '==================================================================================='
+    puts Hash[rule_data.values].class
+    puts '==================================================================================='  end
 end
