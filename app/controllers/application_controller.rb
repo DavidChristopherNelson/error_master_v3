6ae87fea27db41285a44dbe984faafd1ee673c06
David Nelson <david.nelson1@uqconnect.edu.au>
2022-02-23 13:35:24 +1000

    Try .to_a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,13 +8,7 @@ class ApplicationController < ActionController::Base
     sql_rule_command = 'SELECT rules.filter_id, rules.id, rules.field, rules.value FROM filters INNER JOIN rules ON filters.id = rules.filter_id'
     rule_data = Rule.connection.execute(sql_rule_command)
     puts '==================================================================================='
-    puts rule_data
-    p rule_data
-    puts '==================================================================================='
-
-    puts '==================================================================================='
-    puts rule_data.to_a
-    p rule_data
+    p rule_data.to_a
     puts '==================================================================================='
   end
 end
