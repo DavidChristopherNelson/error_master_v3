bcd3c8ef5d7f3736f43dc1ca0acfdebc51198f58
David Nelson <david.nelson1@uqconnect.edu.au>
2022-02-23 14:46:54 +1000

    Testing .to_a and caching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,33 @@ class ApplicationController < ActionController::Base
   include RuleEngine
 
   def home
+    sql_rule_command = 'SELECT rules.filter_id, rules.id, rules.field, rules.value
+                        FROM filters
+                        INNER JOIN rules
+                        ON filters.id = rules.filter_id'
+    rule_data = Rule.connection.execute(sql_rule_command)
+    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    puts "rule_data"
+    p rule_data
+    p rule_data.class
+    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    rule_data_in_array = rule_data.to_a
+    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    puts "rule_data_in_array"
+    p rule_data_in_array
+    p rule_data_in_array.class
+    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+    cached_data = Rails.cache.fetch('rules') do
+      puts "cache miss"
+      rule_data_in_array
+    end
+    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    puts "cached_data"
+    p cached_data
+    p cached_data.class
+    puts "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+
+    puts cached_data
   end
 end
