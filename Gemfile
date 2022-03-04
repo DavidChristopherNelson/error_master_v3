8ea28965256cfee580dc81f32e6050bed194573f
David Nelson <david.nelson1@uqconnect.edu.au>
2022-03-04 17:04:38 +1000

    Awesome print parameters in error view

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
+gem 'awesome_print'
 gem 'bootsnap',   '1.7.2', require: false
 gem 'dalli'
 gem 'jbuilder',   '2.10.0'
--- sandbox.rb
+++ sandbox.rb
@@ -1,45 +1,23 @@
-
 require 'json'
-
-my_object = { :array => [1, 2, 3, { :sample => "hash"} ], :foo => "bar" }
-#puts JSON.pretty_generate(my_object)
-
-error_data = "{\"n\"=>\"88223082\", \"name\"=>\"Perfect-T-Cotton-T-Shirt\", \"from_name\"=>true, \"action\"=>\"create_product\", \"controller\"=>\"home\"}"
-#error_data_hash = JSON(error_data)
-#puts JSON.pretty_generate(error_data_hash)
-
-error_hash = {
-  "n" => "88223082",
-  "name"=>"Perfect-T-Cotton-T-Shirt", 
-  "from_name"=>true, 
-  "action"=>"create_product", 
-  "controller"=>"home"
-}
+require 'pp'
+require 'pry'
+require "awesome_print"
+params = "{\"username\"=>\"SigCon_Deco_Prod\", \"sortby\"=>\"5\", \"controller\"=>\"api/json/manage_inventory\", \"password\"=>\"z8gWv_pd9*HLpz$c\", \"action\"=>\"find\", \"conditions\"=>{\"1\"=>{\"field\"=>\"5\", \"condition\"=>\"6\", \"date1\"=>\"2021-06-10T00:53:58\"}}}"
+params_as_hash = {"username"=>"SigCon_Deco_Prod", "sortby"=>"5", "controller"=>"api/json/manage_inventory", "password"=>"z8gWv_pd9*HLpz$c", "action"=>"find", "conditions"=>{"1"=>{"field"=>"5", "condition"=>"6", "date1"=>"2021-06-10T00:53:58"}}}
 
 
-error_string = "{\"n\":\"88223082\",\"name\":\"Perfect-T-Cotton-T-Shirt\",\"from_name\":true,\"action\":\"create_product\",\"controller\":\"home\"}"
-puts JSON.pretty_generate(JSON(error_string))
+pp JSON(params.gsub('=>', ':'))
+
+puts '-----------------------------------------'
+puts JSON.pretty_generate(JSON(params.gsub('=>', ':')))
+
+puts '-----------------------------------------'
+Pry::ColorPrinter.pp(JSON(params.gsub('=>', ':')))
+
+puts '-----------------------------------------'
+ap JSON(params.gsub('=>', ':')), options = {}
 
 
-puts JSON.pretty_generate(JSON(error_data.gsub("=>", ":")))
-
-flash.now[:notice] = "We
-flash[:message] = "Missing Fields"
-
-{ "action": "design", "ctype": "1", "name": "Lacrosse_Player1", "ct": "0", "from_name": true, "controller": "designs", "c": "22941037", "d": "457869802", "f": "3" }
-
-
-
-@article = Article.new(title: "...", body: "...")
-
-    if @article.save
-
-
-    <%= link_to folder_path(id: @folder.id, rule_engine: true), remote: true do %>
-      <%= image_tag("rule-engine.svg", class: "icon white") %>
-    <% end %>
-
-
-    elsif params['ignore']
-      field = params['ignore_rule']['field']
-      value = params['ignore_rule']['value']
+session_data = "{:set_login_token=>:delete,\n \"user\"=>nil,\n :last_shopping_page=>\n  \"/create_products/Women-s-Oasis-Wash-French-Terry-Hooded-Full-Zip-Sweatshirt?dp=2&n=38694857&pn=1&pn_p=98\",\n :request_count=>1,\n :init_app=>4,\n :ref=>nil,\n :expiry_time=>Mon Aug 16 22:02:19 +1000 2021,\n :validation_key=>\"vecovakofrustaphorastosyprodondususpecrifrymetryly\",\n :d_key=>\"bei.deco-catalog.com\",\n :return_to_is_public=>true,\n :utype=>\"0\",\n \"flash\"=>{},\n :brand_id=>11257953,\n :back_location=>\n  \"http://bei.deco-catalog.com/create_products/Women-s-Oasis-Wash-French-Terry-Hooded-Full-Zip-Sweatshirt?dp=2&n=38694857&pn=1&pn_p=98\",\n :return_to=>\n  \"http://bei.deco-catalog.com/create_products/Women-s-Oasis-Wash-French-Terry-Hooded-Full-Zip-Sweatshirt?dp=2&n=38694857&pn=1&pn_p=98\",\n :init_url=>\n  \"/create_products/Women-s-Oasis-Wash-French-Terry-Hooded-Full-Zip-Sweatshirt?dp=2&n=38694857&pn=1&pn_p=98\",\n :afid=>84543}"
+session_data.gsub!(/:(\w)=>/, /\1/)
+p session_data
